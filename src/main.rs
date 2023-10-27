17fb2d1ed002d25ef0ea0205e9b88b1c214fb74b
Keir Lawson <keirlawson@gmail.com>
2023-10-27 15:17:30 +0100

    Add streamdeck interaction

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use paho_mqtt as mqtt;
 use rodio::{Decoder, OutputStream, Sink};
 use serde::Deserialize;
 use std::fs;
+use std::time::Duration;
 use std::{fs::File, io::BufReader};
+use streamdeck::{pids, Error, StreamDeck};
 
 #[derive(Deserialize)]
 struct BrokerConfig {
@@ -31,17 +33,33 @@ async fn main() {
 
         let client = mqtt::AsyncClient::new(broker_url).unwrap();
 
-        client.connect(Some(con_opts)).await.unwrap();
+        // client.connect(Some(con_opts)).await.unwrap();
 
         println!("Broker connected");
     }
 
+    const ELGATO_VID: u16 = 0x0fd9;
+    let mut deck = StreamDeck::connect(ELGATO_VID, pids::REVISED_MINI, None).unwrap();
+    const POLL_WAIT: Duration = Duration::new(1, 0);
+    loop {
+        let result = deck.read_buttons(Some(POLL_WAIT));
+        match result {
+            Ok(pressed) => handle_press(&pressed),
+            Err(err) => {
+                if !matches!(err, Error::NoData) {
+                    println!("oops");
+                    // bail!(err)
+                }
+            }
+        }
+    }
+}
+
+fn handle_press(pressed: &Vec<u8>) {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let file = BufReader::new(File::open("epicbattle.mp3").unwrap());
     let source = Decoder::new(file).unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
     sink.append(source);
     sink.sleep_until_end();
-
-    println!("Hello, world!");
 }
